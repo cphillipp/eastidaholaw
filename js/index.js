fbefb3845270f4a16ef9587274dd46bc6de81b20
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2012-04-14 01:34:53 -0600

    updated all site pages

--- js/index.js
+++ js/index.js
@@ -1,32 +1,71 @@
-$(function(){
-  $('#slides').slides({
-    preload: true,
-    preloadImage: '../images/loading.gif',
-    play: 5000,
-    pause: 2500,
-    hoverPause: true,
-    animationStart: function(current){
-      $('.caption').animate({
-        bottom:-35
-      },100);
-      if (window.console && console.log) {
-        // example return of current slide number
-        console.log('animationStart on slide: ', current);
-      };
-    },
-    animationComplete: function(current){
-      $('.caption').animate({
-        bottom:0
-      },200);
-      if (window.console && console.log) {
-        // example return of current slide number
-        console.log('animationComplete on slide: ', current);
-      };
-    },
-    slidesLoaded: function() {
-      $('.caption').animate({
-        bottom:0
-      },200);
-    }
+$(document).ready(function() {
+  $(function(){
+    $('#slides').slides({
+      preload: true,
+      preloadImage: '../images/loading.gif',
+      play: 5000,
+      pause: 2500,
+      hoverPause: true,
+      animationStart: function(current){
+        $('.caption').animate({
+          bottom:-35
+        },100);
+        if (window.console && console.log) {
+          // example return of current slide number
+          console.log('animationStart on slide: ', current);
+        };
+      },
+      animationComplete: function(current){
+        $('.caption').animate({
+          bottom:0
+        },200);
+        if (window.console && console.log) {
+          // example return of current slide number
+          console.log('animationComplete on slide: ', current);
+        };
+      },
+      slidesLoaded: function() {
+        $('.caption').animate({
+          bottom:0
+        },200);
+      }
+    });
+  });
+
+  $(".firm-toggle .firm").click(function() {
+    $(".firm-toggle li").removeClass("active");
+    $(".right section").removeClass("active");
+    $(this).addClass("active");
+    $(".right .firm").addClass("active");
+  });
+  $(".firm-toggle .history").click(function() {
+    $(".firm-toggle li").removeClass("active");
+    $(".right section").removeClass("active");
+    $(this).addClass("active");
+    $(".right .history").addClass("active");
+  });
+  $(".firm-toggle .credo").click(function() {
+    $(".firm-toggle li").removeClass("active");
+    $(".right section").removeClass("active");
+    $(this).addClass("active");
+    $(".right .credo").addClass("active");
+  });
+  $(".firm-toggle .diversity").click(function() {
+    $(".firm-toggle li").removeClass("active");
+    $(".right section").removeClass("active");
+    $(this).addClass("active");
+    $(".right .diversity").addClass("active");
+  });
+  $(".firm-toggle .community").click(function() {
+    $(".firm-toggle li").removeClass("active");
+    $(".right section").removeClass("active");
+    $(this).addClass("active");
+    $(".right .community").addClass("active");
+  });
+  $(".firm-toggle .recognition").click(function() {
+    $(".firm-toggle li").removeClass("active");
+    $(".right section").removeClass("active");
+    $(this).addClass("active");
+    $(".right .recognition").addClass("active");
   });
 });
